e2f9f7223a796c4e2fc41e5b06af1452dc33d385
ariskeshav <penandinka@gmail.com>
2017-01-24 19:03:11 -0500

    I made something to say hello world
    I made something to say hello world

---
+++ HelloWorld.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+int age = 20;
+/**
+ *
+ * @author maya
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println(age);
+    }
+    
+}
+//myfirst program! I can make notes after 2 slashes
+
+/**
+*
+*I can put longer messages with slashes and stars like this
+*/
+
+//variableTYpe variableName = some value;
+
+//some types of variables are int, double, char and boolean
+//int stores integers, double stores fractional numbers, char single characters
+// and boolean stores truth values true or false
+
+
